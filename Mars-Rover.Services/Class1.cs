7f7682d86206cfcca181f556bff616df44c7ad0f
DESKTOP-4V0687O\Fatih <fatih.mandirali@hotmail.com>
2021-01-16 09:47:00 +0300

    LocationServices added

--- Mars-Rover.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Mars_Rover.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ Mars-Rover.Services/LocationService.cs
@@ -0,0 +1,13 @@
+﻿using Mars_Rover.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Rover.Services
+{
+    public class LocationService : ILocationService
+    {
+    }
+}
