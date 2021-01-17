52dfb728709a2e14c233af9b9e2845e5a477199a
DESKTOP-4V0687O\Fatih <fatih.mandirali@hotmail.com>
2021-01-17 11:26:52 +0300

    swagger setting

--- Mars-Rover/Startup.cs
+++ Mars-Rover/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -33,6 +35,9 @@ namespace Mars_Rover
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mars_Rover", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
             services.AddMvc().AddJsonOptions(opts =>{opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());});
             services.AddTransient<ILocationService, LocationService>();
